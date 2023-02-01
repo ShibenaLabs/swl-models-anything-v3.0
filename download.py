6b85a43674add61d1aa7140da61ba65dc2277d7c
ShibenaLabs <121632876+ShibenaLabs@users.noreply.github.com>
2023-01-31 20:27:27 -0600

    Update download.py

--- download.py
+++ download.py
@@ -3,17 +3,23 @@
 import os
 import time
 import torch
-from diffusers import StableDiffusionPipeline, EulerAncestralDiscreteScheduler
+from diffusers import StableDiffusionPipeline
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights at build time
     t1 = time.time()
-    model_id = "prompthero/openjourney-v2"
-    model = StableDiffusionPipeline.from_pretrained(
-        model_id
-    )
+    model_id = "swl-models/anything-v3.0"
+    model = StableDiffusionPipeline.from_pretrained(model_id, revision=branch_name, torch_dtype=torch.float16)
+    
     t2 = time.time()
     print("Download took - ",t2-t1,"seconds")
 
 if __name__ == "__main__":
     download_model()
+
+    from diffusers import StableDiffusionPipeline
+
+
+branch_name= "diffusers"
+pipe = pipe.to("cuda")
+
